687e1370d5ae5823e0bb298524b80ffa3ff25f0f
Soltus <liaoshanyi@qq.com>
2023-02-01 17:22:50 +0800

    no message

--- app/src/sillot/vue-toast-notification.ts
+++ app/src/sillot/vue-toast-notification.ts
@@ -4,5 +4,5 @@ import 'vue-toast-notification/dist/theme-sugar.css';
 export function exApp1() {
   // 无需使用 VUE
   const $toast = useToast();
-  window.__toast = $toast;
+  window.__.toast = $toast;
 }
