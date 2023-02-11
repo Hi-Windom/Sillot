6b4f8fd6af50dfa35f6439587ae1797a0e68a10f
Soltus <83791825+Soltus@users.noreply.github.com>
2023-02-12 03:13:10 +0800

    no message

--- app/src/sillot/joyUI/com_/MDeditor.tsx
+++ app/src/sillot/joyUI/com_/MDeditor.tsx
@@ -9,8 +9,10 @@ import ModalClose from "@mui/joy/ModalClose";
 import Typography from "@mui/joy/Typography";
 import Sheet from "@mui/joy/Sheet";
 import Switch from "@mui/joy/Switch";
-import Select from "@mui/joy/Select";
+import Select, { selectClasses } from "@mui/joy/Select";
 import Option from "@mui/joy/Option";
+import Chip from "@mui/joy/Chip";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import loader from "@monaco-editor/loader";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
 import { uriFromPath } from "../../util/path";
@@ -42,6 +44,7 @@ function getCodeFontSizeAria(value: number) {
 
 function CloseModal() {
   const [open, setOpen] = React.useState(true); // react hooks
+  // 在 React 中， <></> 是 <React.Fragment/> 的语法糖
   return (
     <React.Fragment>
       {/* 必须使用CssVarsProvider覆盖样式才会生效 */}
@@ -70,29 +73,83 @@ function CloseModal() {
             }}
           >
             <ModalClose variant="soft" />
-
-            <Select
-              color="primary"
-              placeholder="Mode"
-              defaultValue="KMD"
-              variant="soft"
-              style={{maxWidth:"13em",margin:"2% 0"}}
-            >
-              <Option value="MD">Markdown</Option>
-              <Option value="KMD">KMarkdown</Option>
-            </Select>
+            <Typography component="span">
+              <Select
+                color="primary"
+                placeholder="Mode"
+                defaultValue="KMD"
+                variant="soft"
+                indicator={<KeyboardArrowDown />}
+                endDecorator={
+                  <Chip size="sm" color="danger" variant="soft">
+                    2+
+                  </Chip>
+                }
+                sx={{
+                  width: 240,
+                  [`& .${selectClasses.indicator}`]: {
+                    transition: "0.2s",
+                    [`&.${selectClasses.expanded}`]: {
+                      transform: "rotate(-180deg)",
+                    },
+                  },
+                }}
+                style={{ maxWidth: "13em", margin: "2% 0" }}
+              >
+                <Option value="MD">Markdown</Option>
+                <Option value="KMD">KMarkdown</Option>
+              </Select>
+            </Typography>
             <Checkbox
               color="primary"
               label="及时保存"
               size="md"
               variant="soft"
+              style={{ margin: "0 1em" }}
             />
             <Checkbox
               color="primary"
               label="自动换行"
               size="md"
               variant="soft"
+              style={{ margin: "0 1em" }}
             />
+            <Typography
+              component="label"
+              endDecorator={
+                <Switch
+                  slotProps={{
+                    track: {
+                      children: (
+                        <>
+                          <Typography
+                            component="span"
+                            level="inherit"
+                            sx={{ ml: "10px" }}
+                          >
+                            On
+                          </Typography>
+                          <Typography
+                            component="span"
+                            level="inherit"
+                            sx={{ mr: "8px" }}
+                          >
+                            Off
+                          </Typography>
+                        </>
+                      ),
+                    },
+                  }}
+                  sx={{
+                    "--Switch-thumb-size": "27px",
+                    "--Switch-track-width": "64px",
+                    "--Switch-track-height": "31px",
+                  }}
+                />
+              }
+            >
+              编辑模式
+            </Typography>
             <Slider
               aria-label="Code fontSize"
               defaultValue={16}
@@ -134,7 +191,7 @@ export default class MDDialog {
   public readonly root: any;
   public readonly id: string;
   public readonly nodeID: string;
-  constructor(props: { id: string,nodeID: string}) {
+  constructor(props: { id: string; nodeID: string }) {
     this.id = props.id;
     this.nodeID = props.nodeID;
     let e = document.getElementById(props.id);
@@ -162,27 +219,37 @@ export default class MDDialog {
     /// #endif
     loader.init().then((monacoInstance) => {
       console.log("Here is the monaco instance", monacoInstance);
-      window.sout.info(this.nodeID)
-      fetchPost("/api/block/getBlockKramdown", {
-        "id": this.nodeID
-      }, res => {
-        window.sout.success(res)
-        if (res.code == 0) {
-          let data = res.data.kramdown
-          let editor = monacoInstance.editor.create(document.getElementById("monaco-editor"), {
-            value: data,
-            contextmenu: true,
-            language: "markdown",
-            theme: "vs-dark",
-            automaticLayout: true
-          });
-          window.sout.tracker(editor);
+      window.sout.info(this.nodeID);
+      fetchPost(
+        "/api/block/getBlockKramdown",
+        {
+          id: this.nodeID,
+        },
+        (res) => {
+          window.sout.success(res);
+          if (res.code == 0) {
+            let data = res.data.kramdown;
+            let editor = monacoInstance.editor.create(
+              document.getElementById("monaco-editor"),
+              {
+                value: data,
+                contextmenu: true,
+                language: "markdown",
+                theme: "vs-dark",
+                automaticLayout: true,
+              }
+            );
+            window.sout.tracker(editor);
+          } else {
+            this.root.render();
+            window.__.toastify.error({
+              message: res.msg,
+              position: "bottom-center",
+              duration: 1,
+            });
+          }
         }
-        else {
-          this.root.render()
-          window.__.toastify.error({message:res.msg,position:"bottom-center",duration:1});
-        }
-      })
+      );
     });
   }
 }
