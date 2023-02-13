1f15ba5867662540c4d4496a4ece41f8967a9192
Soltus <83791825+Soltus@users.noreply.github.com>
2023-02-14 03:42:24 +0800

    #74 替换入口

--- app/src/layout/status.ts
+++ app/src/layout/status.ts
@@ -36,9 +36,13 @@ export const initStatus = (isWindow = false) => {
 <div class="fn__flex-1"></div>
 <div class="status__backgroundtask fn__none"></div>
 <div class="status__counter"></div>
-<div id="statusHelp" class="toolbar__item b3-tooltips b3-tooltips__w" aria-label="${window.siyuan.languages.help}">
+<div id="statusHelp" class="toolbar__item b3-tooltips b3-tooltips__w fn__none" aria-label="${window.siyuan.languages.help}">
     <svg><use xlink:href="#iconHelp"></use></svg>
+</div>
+<div id="SillotDrawer" class="toolbar__item b3-tooltips b3-tooltips__w" aria-label="Sillot Drawer">
+    <svg><use xlink:href="#iconList"></use></svg>
 </div>`;
+
     if (!isWindow) {
         // 由于点击切换时需隐藏菜单，因此不可使用 CSS
         const dockElement = document.getElementById("barDock");
