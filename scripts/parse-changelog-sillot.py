10281716bfee792790f229124c798b41a213ac59
Soltus <83791825+Soltus@users.noreply.github.com>
2023-02-15 05:48:53 +0800

    no message

--- scripts/parse-changelog-sillot.py
+++ scripts/parse-changelog-sillot.py
@@ -59,13 +59,64 @@ def find_milestone(repo, title):
     version = ".".join(pat.group(1).split(".")[:2])
     print('''
 ---
-如果你也热爱思源，欢迎一起共建汐洛分支❤️ 694357845@qq.com
---
-海文东QQ频道：43in2c1qb9 同步更新安装包
---
+❤️ 如果你也热爱思源，欢迎一起共建汐洛分支 694357845@qq.com
+💠 海文东QQ频道：43in2c1qb9 异步更新安装包
 🐱‍🏍 Sillot is currently in active development
 ⚠️ 这是自动构建的开发者版本！数据无价，请勿用于生产环节
 --
+🌏 绛亽新时代智慧彖乄同全球开源力量一同见证人类命运共同体
+--
+> 这只是一份赞扬清单，列出并不代表在海文东项目中实际应用，请以项目开源声明为准
+
+### 🥇 金牌开源力量
+
+- [monaco-editor](https://github.com/microsoft/monaco-editor)
+- [am-editor](https://github.com/red-axe/am-editor)
+- [MaterialDesignInXamlToolkit](https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit)
+- [React](https://github.com/facebook/react)
+
+### 🥈 银牌开源力量
+
+- [daisyui](https://github.com/saadeghi/daisyui)
+- [astro](https://github.com/withastro/astro)
+- [nivo](https://github.com/plouc/nivo)
+- [recharts](https://github.com/recharts/recharts)
+- [mobx](https://github.com/mobxjs/mobx)
+- [react-markdown](https://github.com/remarkjs/react-markdown)
+- [alist](https://github.com/alist-org/alist)
+- [obsidian-various-complements-plugin](https://github.com/tadashi-aikawa/obsidian-various-complements-plugin)
+
+### 🥉 铜牌开源力量
+
+- [relation-graph](https://github.com/seeksdream/relation-graph)
+- [rqlite](https://github.com/rqlite/rqlite)
+- [tweakpane](https://github.com/cocopon/tweakpane)
+- [yq](https://github.com/mikefarah/yq)
+- [react-simple-keyboard](https://github.com/hodgef/react-simple-keyboard)
+- [lyra](https://github.com/LyraSearch/lyra)
+- [limitPNG](https://github.com/nullice/limitPNG)
+- [react-grid-layout](https://github.com/react-grid-layout/react-grid-layout)
+- [react-spinners](https://github.com/davidhu2000/react-spinners)
+- [react-medium-image-zoom](https://github.com/rpearce/react-medium-image-zoom)
+- [react-fast-marquee](https://github.com/justin-chu/react-fast-marquee)
+- [react-color](https://github.com/casesandberg/react-color)
+- [debugtron](https://github.com/pd4d10/debugtron)
+- [react-music-player](https://github.com/lijinke666/react-music-player)
+- [react-motion](https://github.com/chenglou/react-motion)
+- [motion](https://github.com/framer/motion)
+- [react-hot-toast](https://github.com/timolins/react-hot-toast)
+- [react-toastify](https://github.com/fkhadra/react-toastify)
+- [pixi-react](https://github.com/pixijs/pixi-react)
+- [react-modern-calendar-datepicker](https://github.com/Kiarash-Z/react-modern-calendar-datepicker)
+- [react-loading-skeleton](https://github.com/dvtng/react-loading-skeleton)
+- [driver.js](https://github.com/kamranahmedse/driver.js)
+- [ace](https://github.com/ajaxorg/ace)
+- [sweetalert2](https://github.com/sweetalert2/sweetalert2)
+- [rough-notation](https://github.com/rough-stuff/rough-notation)
+- [ClickShow](https://github.com/cuiliang/ClickShow)
+
+---
+
 [![CI](https://github.com/Hi-Windom/Sillot/actions/workflows/ci.yml/badge.svg)](https://github.com/Hi-Windom/Sillot/actions/workflows/ci.yml)<img src="https://img.shields.io/badge/Windows 10+-blue?logo=Windows 11" title="" style="margin: 3px auto;"/><img src="https://img.shields.io/badge/macOS-purple?logo=macOS" title="" style="margin: 3px auto;"/>
 ''')
     for milestone in repo.get_milestones():
