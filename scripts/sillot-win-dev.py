106edc86ec53753aaaa650bc01be2b00856d8b72
Soltus <liaoshanyi@qq.com>
2023-03-23 17:42:55 +0800

    #331

--- scripts/sillot-win-dev.py
+++ scripts/sillot-win-dev.py
@@ -3,7 +3,7 @@ import subprocess
 root = os.path.dirname(os.path.dirname(__file__))
 app = os.path.join(root, 'app')
 kernel = os.path.join(app, 'kernel')
-os.system('taskkill /f /t /im SiYuan.exe')
+os.system('taskkill /f /t /im Sillot.exe')
 os.system('taskkill /f /t /im SiYuan-Sillot-Kernel.exe')
 print("\n---")
 print("{0} -> call SiYuan-Sillot-Kernel.exe --wd=.. --mode=dev".format(kernel))
