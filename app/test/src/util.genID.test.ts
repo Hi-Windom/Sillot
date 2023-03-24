e3ab65318870c8ab5b61fe46ab1d9f1c0d6b17e0
Soltus <liaoshanyi@qq.com>
2023-03-25 00:33:02 +0800

    no message

---
+++ app/test/src/util.genID.test.ts
@@ -0,0 +1,5 @@
+import {genUUID} from "../../src/util/genID";
+
+test("test genUUID",()=>{
+  expect(genUUID()).toMatch(/^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$/g);
+});
