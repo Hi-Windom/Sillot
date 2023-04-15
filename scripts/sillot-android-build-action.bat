1068fa1f2eeeb0b47fdf1dd921adc3524f9e626c
Soltus <liaoshanyi@qq.com>
2023-04-15 11:57:21 +0800

    test https://github.com/ci-win/siyuan/issues/3

--- scripts/sillot-android-build-action.bat
+++ scripts/sillot-android-build-action.bat
@@ -1,17 +1,10 @@
 @echo off
-echo 'use ".\scripts\sillot-android-build.bat" instead of "sillot-android-build.bat"'
-
-echo ===== Building UI =====
-cd app
-call pnpm install
-call pnpm run build
-cd ..
 
 echo ===== Building Kernel lib =====
 cd kernel
 @REM $env:GOPROXY="https://mirrors.aliyun.com/goproxy/,direct"
-@REM go install golang.org/x/mobile/cmd/gomobile@latest
-@REM gomobile init
+go install golang.org/x/mobile/cmd/gomobile@latest
+gomobile init
 gomobile bind --tags fts5 -ldflags "-s -w" -v -o kernel.aar -target="android/arm64" -androidapi 30 ./mobile/
 cd ..
 python .\scripts\sillot-android-dev.py
