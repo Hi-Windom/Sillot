3cdaab9c05bb1d4789a0b3e5541428056d452bfa
Soltus <liaoshanyi@qq.com>
2023-04-15 18:03:54 +0800

    no message

--- scripts/sillot-android-build-action.bat
+++ scripts/sillot-android-build-action.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo ===== Building Kernel lib =====
-cd kernel
-@REM $env:GOPROXY="https://mirrors.aliyun.com/goproxy/,direct"
-go install golang.org/x/mobile/cmd/gomobile@latest
-gomobile init
-gomobile bind --tags fts5 -ldflags "-s -w" -v -o kernel.aar -target="android/arm64" -androidapi 30 ./mobile/
-cd ..
+@REM cd kernel
+@REM @REM $env:GOPROXY="https://mirrors.aliyun.com/goproxy/,direct"
+@REM go install golang.org/x/mobile/cmd/gomobile@latest
+@REM gomobile init
+@REM gomobile bind --tags fts5 -ldflags "-s -w" -v -o kernel.aar -target="android/arm64" -androidapi 30 ./mobile/
+@REM cd ..
 python .\scripts\sillot-android-dev.py
 echo ===== all jobs done. =====
