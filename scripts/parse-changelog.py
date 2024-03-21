4917e183696f253854adfbc455f481e2a064b145
Soltus <liaoshanyi@qq.com>
2024-03-22 00:07:18 +0800

    试试输出两个思源版本issue

--- scripts/parse-changelog.py
+++ scripts/parse-changelog.py
@@ -29,21 +29,27 @@ def generate_msg_from_repo(repo_name, tag_name, lastestRelease):
     """
     hostname = os.getenv("GITHUB_HOST") or "api.github.com"
     token = os.getenv("GITHUB_TOKEN")
-    desc_mapping = defaultdict(list)
 
     gh = github.Github(token, base_url=f"https://{hostname}")
     repo = gh.get_repo(repo_name)
-    milestone = find_milestone(repo, tag_name, lastestRelease)
+    print('## [@SiYuan](https://github.com/siyuan-note/siyuan)\n')
+    for v in [tag_name, lastestRelease]:
+      print(f'''---
 
-    for issue in repo.get_issues(state="closed", milestone=milestone):  # type: ignore
-        # REF https://pygithub.readthedocs.io/en/latest/github_objects/Issue.html#github.Issue.Issue
-        desc_mapping[get_issue_first_label(issue)].append(
-            {"title": issue.title, "url": issue.html_url}
-        )
-    generate_msg(desc_mapping)
+### v{v}
+
+''')
+      desc_mapping = defaultdict(list)
+      milestone = find_milestone(repo, v)
+      for issue in repo.get_issues(state="closed", milestone=milestone):  # type: ignore
+          # REF https://pygithub.readthedocs.io/en/latest/github_objects/Issue.html#github.Issue.Issue
+          desc_mapping[get_issue_first_label(issue)].append(
+              {"title": issue.title, "url": issue.html_url}
+          )
+      generate_msg(desc_mapping)
 
 
-def find_milestone(repo, title, lastestRelease):
+def find_milestone(repo, title):
     """Find the milestone in a repository that is similar to milestone title
 
     Args:
@@ -91,7 +97,6 @@ def get_issue_first_label(issue):
 def generate_msg(desc_mapping):
     """Print changelogs from direction."""
     print()
-    print('## [@SiYuan](https://github.com/siyuan-note/siyuan)\n')
     for header in docmap:
         if not desc_mapping[header]:
             continue
