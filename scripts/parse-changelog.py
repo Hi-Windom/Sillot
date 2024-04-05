f2df9eb38a47a58c6087a89ba6b725bed9553760
Soltus <liaoshanyi@qq.com>
2024-04-05 19:07:57 +0800

    test

--- scripts/parse-changelog.py
+++ scripts/parse-changelog.py
@@ -1,5 +1,5 @@
 import os
-import re
+import re, ast
 from argparse import ArgumentParser
 from collections import defaultdict
 
@@ -15,8 +15,14 @@ docmap = {
     # "Development": "Development",
 }
 
+def quote_versions_v2(s):
+    """
+    支持一个或两个点号分隔的版本号，请确保格式正确，本函数不做校验
+    字符串示例: "[3.0.7, 3.0.6, 3.0, 3.1]"
+    """
+    return ast.literal_eval(re.sub(r'(\b\d+(\.\d+){1,2}\b)', r'"\1"', s))
 
-def generate_msg_from_repo(repo_name, tag_name, lastestRelease):
+def generate_msg_from_repo(repo_name, tag_name, otherReleaseArray):
     """Generate changelog messages from repository and tag name.
 
     Envs:
@@ -33,20 +39,27 @@ def generate_msg_from_repo(repo_name, tag_name, lastestRelease):
     gh = github.Github(token, base_url=f"https://{hostname}")
     repo = gh.get_repo(repo_name)
     print('# [@SiYuan](https://github.com/siyuan-note/siyuan)\n')
-    for v in [tag_name, lastestRelease]:
-      print(f'''
+    print(f'''
 
-## ⚓ [{v}](https://github.com/siyuan-note/siyuan/releases/tag/{v}) 主要变化
+## ⚓ [{tag_name}](https://github.com/siyuan-note/siyuan/releases/tag/{tag_name}) 主要变化
 
 ''')
-      desc_mapping = defaultdict(list)
-      milestone = find_milestone(repo, v)
-      for issue in repo.get_issues(state="closed", milestone=milestone):  # type: ignore
+    desc_mapping = defaultdict(list)
+    milestone = find_milestone(repo, tag_name)
+    for issue in repo.get_issues(state="closed", milestone=milestone):  # type: ignore
           # REF https://pygithub.readthedocs.io/en/latest/github_objects/Issue.html#github.Issue.Issue
-          desc_mapping[get_issue_first_label(issue)].append(
+        desc_mapping[get_issue_first_label(issue)].append(
               {"title": issue.title, "url": issue.html_url}
           )
-      generate_msg(desc_mapping)
+    generate_msg(desc_mapping)
+    arr = quote_versions_v2(otherReleaseArray)
+    if len(arr) > 0:
+        for v in arr:
+            print(f'''
+
+## ✅ [{v}](https://github.com/siyuan-note/siyuan/releases/tag/{v}) 也包含在此次更新💫
+
+''')
 
 
 def find_milestone(repo, title):
@@ -110,12 +123,12 @@ if __name__ == "__main__":
     parser = ArgumentParser(
         description="Automaticly generate information from issues by tag."
     )
-    parser.add_argument("-t", "--tag", help="the tag to filter issues.")
-    parser.add_argument("-b", "--lastestRelease", help="lastest Release")
+    parser.add_argument("-t", "--tag", help="the version tag to filter issues.")
+    parser.add_argument("-a", "--otherReleaseArray", help="the version array for other Release")
     parser.add_argument("repo", help="The repository name")
     args = parser.parse_args()
 
     try:
-        generate_msg_from_repo(args.repo, args.tag, args.lastestRelease)
+        generate_msg_from_repo(args.repo, args.tag, args.otherReleaseArray)
     except AssertionError:
         print(args.tag)
