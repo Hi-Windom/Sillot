c02eabe9dff1a907db8c2f3bd089998fa0b90767
Soltus <liaoshanyi@qq.com>
2024-04-25 17:37:42 +0800

    改进 Sillot CI/CD 工作流 #655

--- scripts/sillot-android-build.bat
+++ scripts/sillot-android-build.bat
@@ -5,6 +5,9 @@ echo ===== Building UI =====
 cd app
 call pnpm install
 call pnpm run build
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
 cd ..
 
 echo ===== Building Kernel lib =====
@@ -13,6 +16,12 @@ cd kernel
 @REM go install golang.org/x/mobile/cmd/gomobile@latest
 @REM gomobile init
 gomobile bind --tags fts5 -ldflags "-s -w" -v -o kernel.aar -target="android/arm64" -androidapi 30 ./mobile/
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
 cd ..
 python .\scripts\sillot-android-dev.py
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
 echo ===== all jobs done. =====
