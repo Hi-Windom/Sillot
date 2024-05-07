4f003cbeb8db7ae3c72d0fa30de26e521fd63d93
Soltus <liaoshanyi@qq.com>
2024-05-07 11:54:37 +0800

    初始化大版本

--- kernel/util/working.go
+++ kernel/util/working.go
@@ -45,9 +45,9 @@ import (
 var Mode = "prod"
 
 const (
-	Ver       = "0.32"
+	Ver       = "0.33"
 	VerC      = Ver + ".999" // 用于检查版本更新
-	VerSY     = "3.0.13"     // 思源版本号
+	VerSY     = "3.0.14"     // 思源版本号
 	IsInsider = true
 	VerDeno   = "1.32.5"
 )
