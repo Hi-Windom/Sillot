41f01b845a12852a8eef18cd962a721f8cd5a175
Soltus <liaoshanyi@qq.com>
2024-05-15 15:00:22 +0800

    ts文件悬浮支持window.siyuan.languages #672

--- .vscode/extensions/Sillot/src/extension.const.ts
+++ .vscode/extensions/Sillot/src/extension.const.ts
@@ -1,5 +1,13 @@
 export const C = {
     extensionId: "Hi-Windom.sillot",
+    extensionName: "sillot",
     npmjs: "https://www.npmjs.com",
     PackageJsonMapping: "PackageJsonMapping.sillot.jsonc",
 }
+
+export const settings = {
+
+};
+export const actions = {
+	enableCodeLens: `${C.extensionName}.`,
+};
--- .vscode/extensions/Sillot/src/extension.ts
+++ .vscode/extensions/Sillot/src/extension.ts
@@ -42,6 +42,7 @@ import { add_module_git_emoji_zh } from "./modules/git_emoji_zh";
 import { add_task_unwantedRecommendations_check_by_id } from "./task/unwantedRecommendations";
 import { loadCompletionItemsFromFile, saveCompletionItemsToFile } from "./utils/json";
 import { add_task_同步更新版本 } from "./task/同步更新版本";
+import { registerHoverProvider_window_siyuan_languages } from "./provider/typescript";
 
 let lastChangedDocument: vscode.TextDocument | null = null;
 let myWebviewPanel: vscode.WebviewPanel | undefined;
@@ -77,6 +78,7 @@ export async function activate(context: vscode.ExtensionContext) {
     );
     add_module_git_emoji_zh(context);
     add_task_同步更新版本(context);
+    registerHoverProvider_window_siyuan_languages(context);
 
     const addMappingDisposable = vscode.commands.registerCommand("汐洛.addPackageJsonMapping", async (uri: vscode.Uri) => {
         if (uri?.fsPath.endsWith("package.json")) {
