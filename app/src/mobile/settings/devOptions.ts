9ef9fdf59c6dc902e3ec9b2468550fe488630296
Soltus <liaoshanyi@qq.com>
2024-06-05 11:22:06 +0800

    手机端菜单项退出应用替换为开发者选项 #810

--- app/src/mobile/settings/devOptions.ts
+++ app/src/mobile/settings/devOptions.ts
@@ -22,6 +22,24 @@ export const initDevOptions = () => {
                         event.stopPropagation();
                         break;
                     }
+                    if (target.id.startsWith("JSAndroid_")) {
+                        const functionName = target.id.replace("JSAndroid_", "");
+                        console.log("target: window.JSAndroid.", functionName)
+                         // 使用Object.prototype.hasOwnProperty来检查函数是否存在
+                        if (Object.prototype.hasOwnProperty.call(window.JSAndroid, functionName) && typeof window.JSAndroid[functionName] === 'function') {
+                            // 调用对应的函数
+                            window.JSAndroid[functionName]();
+                        }
+                        event.preventDefault();
+                        event.stopPropagation();
+                        break;
+                    }
+                    // if (target.id === "JSAndroid_androidReboot") {
+                    //     window.JSAndroid?.androidReboot();
+                    //     event.preventDefault();
+                    //     event.stopPropagation();
+                    //     break;
+                    // }
                 }
             })
         }
@@ -30,21 +48,39 @@ export const initDevOptions = () => {
 
 function genHTML() {
     return /*html*/ `
-<div class="b3-label">
-    切换 vConsole 浮标是否显示
-    <div class="fn__hr"></div>
-    <button class="b3-button b3-button--outline fn__block" id="toggle_vConsole">
-        <svg><use xlink:href="#iconTerminal"></use></svg> 切换 vConsole 浮标是否显示
-    </button>
-    <div class="b3-label__text">如果已经显示，则隐藏；如果已经隐藏，则显示。</div>
-</div>
-<div class="b3-label">
-    使用非捆绑的前端产物
-    <div class="fn__hr"></div>
-    <button disabled class="b3-button b3-button--outline fn__block" id="toggle_vConsole">
-    使用非捆绑的前端产物
-    </button>
-    <div class="b3-label__text">TODO</div>
+<div>
+    <div class="b3-label">
+        切换 vConsole 浮标是否显示
+        <div class="fn__hr"></div>
+        <button class="b3-button b3-button--outline fn__block" id="toggle_vConsole">
+            <svg><use xlink:href="#iconTerminal"></use></svg> 切换 vConsole 浮标是否显示
+        </button>
+        <div class="b3-label__text">如果已经显示，则隐藏；如果已经隐藏，则显示。</div>
+    </div>
+    <div class="b3-label">
+        使用非捆绑的前端产物
+        <div class="fn__hr"></div>
+        <button disabled class="b3-button b3-button--outline fn__block" id="unbundled_web">
+        使用非捆绑的前端产物
+        </button>
+        <div class="b3-label__text">TODO</div>
+    </div>
+    <div class="b3-label">
+        JSAndroid 接口测试
+        <div class="fn__hr"></div>
+        <button class="b3-button b3-button--outline fn__block" id="JSAndroid_androidReboot">
+        androidReboot
+        </button>
+        <div class="fn__hr"></div>
+        <button class="b3-button b3-button--outline fn__block" id="JSAndroid_restartSillotAndroid">
+        restartSillotAndroid
+        </button>
+        <div class="fn__hr"></div>
+        <button class="b3-button b3-button--outline fn__block" id="JSAndroid_exitSillotAndroid">
+        exitSillotAndroid
+        </button>
+        <div class="b3-label__text">嘿嘿嘿</div>
+    </div>
 </div>
     `
 }
