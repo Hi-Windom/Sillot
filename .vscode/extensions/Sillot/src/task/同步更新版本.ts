614b1d3790df1e2a6078221f928bd3775ac2bf6a
Soltus <liaoshanyi@qq.com>
2024-06-12 14:20:33 +0800

    汐洛VSC扩展add_task_同步更新版本使用格式化时间补全二段版本号 #824

--- .vscode/extensions/Sillot/src/task/同步更新版本.ts
+++ .vscode/extensions/Sillot/src/task/同步更新版本.ts
@@ -4,6 +4,7 @@ import path, { resolve } from "path";
 import { Log } from "../utils/log";
 import { C } from "../extension.const";
 import { loadCompletionItemsFromFile } from "../utils/json";
+import { formatDate } from "sofill/mid";
 
 export function add_task_同步更新版本(context: vscode.ExtensionContext) {
     const TAG = "汐洛.同步更新版本";
@@ -55,23 +56,37 @@ export function add_task_同步更新版本(context: vscode.ExtensionContext) {
             Promise.all(versionPromises).then(versions => {
                 // 过滤掉null值
                 const validVersions = versions.filter(version => version !== null);
-                vscode.window.showInputBox({ prompt: `输入新版本: (当前版本: ${validVersions.join(", ")})` }).then(async version => {
-                    if (version) {
-                        // 遍历映射并更新版本号
-                        selectedOptions.forEach(async (value: string, index: number) => {
-                            Log.d(TAG, value);
-                            if (await fs.exists(value)) {
-                                const pkgContent = fs.readJSONSync(value);
-                                pkgContent.version = version;
-                                fs.writeFileSync(value, JSON.stringify(pkgContent, null, 2));
-                                Log.d(`${version} ->${value}`);
-                            } else {
-                                vscode.window.showWarningMessage(`已跳过无效映射 ${value}`);
+                vscode.window
+                    .showInputBox({
+                        title: "请输入新版本号",
+                        value: validVersions[0],
+                        placeHolder: "如果是二段版本号会被使用格式化时间填充为三段版本号",
+                        prompt: `旧版本号: ${validVersions.join(", ")})`,
+                    })
+                    .then(async version => {
+                        if (version) {
+                            // 检查 version 是否只有两段
+                            const versionParts = version.split(".");
+                            if (versionParts.length === 2) {
+                                // 使用当前时间生成第三段版本号
+                                const timestamp = formatDate(new Date(), "yyyyMMddHHmmss");
+                                version = `${version}.${timestamp}`;
                             }
-                        });
-                        vscode.window.showInformationMessage("所有 package.json 文件的版本已更新。");
-                    }
-                });
+                            // 遍历映射并更新版本号
+                            selectedOptions.forEach(async (value: string, index: number) => {
+                                Log.d(TAG, value);
+                                if (await fs.exists(value)) {
+                                    const pkgContent = fs.readJSONSync(value);
+                                    pkgContent.version = version;
+                                    fs.writeFileSync(value, JSON.stringify(pkgContent, null, 2));
+                                    Log.d(`${version} ->${value}`);
+                                } else {
+                                    vscode.window.showWarningMessage(`已跳过无效映射 ${value}`);
+                                }
+                            });
+                            vscode.window.showInformationMessage("所有 package.json 文件的版本已更新。");
+                        }
+                    });
             });
         } else {
             vscode.window.showWarningMessage("当前不在工作区环境");
