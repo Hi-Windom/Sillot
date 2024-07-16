9b2f731b7f17378735077af789666747c09d5f99
Soltus <liaoshanyi@qq.com>
2024-07-16 14:04:50 +0800

    #883

--- scripts/_sillot-gibbet-kernel-android.bat
+++ scripts/_sillot-gibbet-kernel-android.bat
@@ -1,2 +1,3 @@
 @echo off
-gomobile bind --tags fts5 -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -v -o kernel.aar -target="android/arm64" -androidapi 30 ./mobile/
+@REM -androidapi 一般与 minSdkVersion 对齐
+gomobile bind --tags fts5 -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -v -o kernel.aar -target="android/arm64" -androidapi 31 ./mobile/
