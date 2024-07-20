226988d9141259a0be98ed5ffe63b5aa1dd5121c
Soltus <liaoshanyi@qq.com>
2024-07-20 20:48:48 +0800

    Revert "终止linux平台支持 #850"
    This reverts commit 6cc7e36ab68c6d61fd85606ba637ef2afd536613.

---
+++ scripts/darwin-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo 'Building UI'
+cd app
+pnpm install && pnpm run build
+cd ..
+
+echo 'Cleaning Builds'
+rm -rf app/build
+rm -rf app/kernel-darwin
+rm -rf app/kernel-darwin-arm64
+
+echo 'Building Kernel'
+
+cd kernel
+go version
+export GO111MODULE=on
+export GOPROXY=https://goproxy.io
+export CGO_ENABLED=1
+
+echo 'Building Kernel amd64'
+export GOOS=darwin
+export GOARCH=amd64
+go build --tags fts5 -v -o "../app/kernel-darwin/SiYuan-Sillot-Kernel" -ldflags "-s -w" .
+
+echo 'Building Kernel arm64'
+export GOOS=darwin
+export GOARCH=arm64
+go build --tags fts5 -v -o "../app/kernel-darwin-arm64/SiYuan-Sillot-Kernel" -ldflags "-s -w" .
+cd ..
+
+echo 'Building Electron App amd64'
+cd app
+pnpm run dist-darwin
+echo 'Building Electron App arm64'
+pnpm run dist-darwin-arm64
+cd ..
