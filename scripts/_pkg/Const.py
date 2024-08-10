2a2e5a0ecee82b687e6c3e2af689a815d544d508
Soltus <liaoshanyi@qq.com>
2024-08-10 23:12:18 +0800

    docker hub 使用发行渠道 #851

--- scripts/_pkg/Const.py
+++ scripts/_pkg/Const.py
@@ -30,7 +30,7 @@ HEADER_sillot = '''
 
 # 🚧 汐洛仅用于开发者测试，不要用来存储重要数据！
 
-# ❄️ [国内用户推荐使用阿里云盘下载](https://sillot.db.sc.cn/汐洛宝典/下载/#阿里云盘)
+> 如果 Github 下载困难，可以右键复制 assets 的 apk 文件链接到 [Github 文件加速](https://tool.mintimate.cn/gh/) 下载
 
 <p>
 <img src="https://img.shields.io/badge/Chromium 114+-black?logo=Google Chrome" title="Chromium" height="25"/>
